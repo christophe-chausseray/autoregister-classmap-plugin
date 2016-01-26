7aecd39a2d52e248fe45dcd4d5cbc84b234c4fee
Christophe Chausseray <christophec@iron-mail.net>
2016-01-26 18:32:45 +0000

    Add finder to retrieve register files

--- lib/AutoregisterClassmapPlugin.php
+++ lib/AutoregisterClassmapPlugin.php
@@ -4,10 +4,14 @@ namespace Chris\Composer\AutoregisterClassmapPlugin;
 
 use Chris\Composer\AutoregisterClassmapPlugin\Container\ModuleContainer;
 use Composer\Composer;
+use Composer\EventDispatcher\Event;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
+use Composer\Plugin\CommandEvent;
 use Composer\Plugin\PluginInterface;
+use Symfony\Component\Finder\Finder;
 
-class AutoregisterClassmapPlugin implements PluginInterface
+class AutoregisterClassmapPlugin implements PluginInterface, EventSubscriberInterface
 {
     /**
      * @var string COMPOSER_CONFIG_KEY_EXTRA
@@ -20,11 +24,39 @@ class AutoregisterClassmapPlugin implements PluginInterface
     protected $moduleContainer;
 
     /**
+     * @var Finder
+     */
+    protected $finder;
+
+    /**
      * {@inheritdoc}
      */
     public function activate(Composer $composer, IOInterface $io)
     {
-        $extra = $composer->getPackage()->getExtra();
+        $extra                 = $composer->getPackage()->getExtra();
         $this->moduleContainer = new ModuleContainer($extra[static::COMPOSER_CONFIG_KEY_EXTRA]['path'], $extra[static::COMPOSER_CONFIG_KEY_EXTRA]['filename']);
+        $this->finder          = new Finder();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'pre-autoload-dump' => [
+                ['run', 0]
+            ]
+        ];
+    }
+
+    /**
+     * @param Event $event
+     */
+    public function run(CommandEvent $event)
+    {
+        $this->finder->files()->name($this->moduleContainer->getFilename())->in($this->moduleContainer->getPath());
+
+        var_dump($this->finder);die;
     }
 }
